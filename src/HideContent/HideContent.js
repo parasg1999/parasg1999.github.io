bc8b1cdc0d412cd6d4591e95dfdf3ba6e96493b0
Paras Gupta <parasg1999@gmail.com>
2021-01-06 23:59:11 +0530

    hide content component

---
+++ src/HideContent/HideContent.js
@@ -0,0 +1,55 @@
+/* eslint react/no-unknown-property: 0 */
+/* eslint react/prefer-stateless-function: 0 */
+
+import React, { useState } from 'react'
+import './HideContent.scss'
+
+// Size presets, defined by Spotify
+// const sizePresets = {
+//     large: {
+//         width: 300,
+//         height: 380,
+//     },
+//     compact: {
+//         width: 300,
+//         height: 80,
+//     },
+// }
+
+const HideContent = (props) => {
+    const [hide, setHide] = useState(true);
+    const [pass, setPass] = useState('');
+
+    const handlePassChange = (event) => {
+        setPass(event.target.value);
+        console.log(event.target.value, props.myKey);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (pass === props.myKey) {
+            setHide(false);
+        }
+    }
+
+
+    return (
+        <div className="hideContent">
+            { hide ?
+                (
+                    <form onSubmit={handleSubmit}>
+                        <input value={pass} onChange={handlePassChange} type="search" placeholder="Locked" />
+                    </form>
+                )
+                :
+                (
+                    <>
+                        {props.children}
+                    </>
+                )
+            }
+        </div>
+    )
+}
+
+export default HideContent
