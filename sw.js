3fb52c50c138b00baf299f40a24d29ec139fd63f
Paras Gupta <parasg1999@gmail.com>
2021-01-09 13:02:08 +0530

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3bc3d790d5e5f8a673ae.js"
+    "url": "webpack-runtime-612b6ea7c28871be1d5a.js"
   },
   {
     "url": "styles.e0ac38e0cc5922eee71e.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2e0a280fb51a55d519d01848b3dddb0d"
+    "revision": "38bbdf0b7024b053decbfdc29e1508f2"
   },
   {
     "url": "polyfill-aa31a558d1e59c5d37ec.js"
